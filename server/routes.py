34b3cb8de3df442c7e286555bf1d524bce325d1a
tsuru-mitsu <78587771+tsuru-mitsu@users.noreply.github.com>
2025-12-29 10:36:21 +0900

    Update routes.py
    tes2

--- server/routes.py
+++ server/routes.py
@@ -13,7 +13,7 @@ def index():
 
     if name:
         cursor.execute(
-            "SELECT * FROM books WHERE name LIKE %s", namef
+            "SELECT * FROM books WHERE name LIKE %s", name
         )
         books = [Book(*row) for row in cursor]
 
